ecabd78ed4dfe9085a6857e35dedc9382d293083
dy <jjchrly@gmail.com>
2022-07-02 16:25:42 +0900

    s

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 buildInfo=`date +%F_%H-%M`'_'`git rev-parse --verify HEAD --short=12`
  
 echo "building..." $buildInfo
-docker build -f ./docker/app.Dockerfile -t server:latest .
+docker build -f ./docker/app.Dockerfile -t me-test:latest .
 wait
